f086f40d4806abe66042b4204de7b58fff90ec7f
Mohamed Zayed <m_zayed@hotmail.com>
2018-09-04 22:42:34 -0400

    implementing bubble sort in Python

---
+++ bubble_sort.py
@@ -0,0 +1,10 @@
+def bubble_sort(arr):
+    # For every element (arranged backwards)
+    for n in range(len(arr)-1,0,-1):
+        #
+        for k in range(n):
+            # If we come to a point to switch
+            if arr[k]>arr[k+1]:
+                temp = arr[k]
+                arr[k] = arr[k+1]
+                arr[k+1] = temp
